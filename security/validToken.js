afbedfe8f743518ca3a5c75cd9edd4aa3aacaf57
Taps73r <povroznykmisha@gmail.com>
2024-03-23 00:01:23 +0200

    add holidays to endpoint and update event date endpoint

--- security/validToken.js
+++ security/validToken.js
@@ -4,14 +4,16 @@ function authenticateToken(req, res, next) {
   const token = req.headers.authorization
     ? req.headers.authorization.split(" ")[1]
     : null;
-    
+
   if (!token) {
     return res.status(401).json({ message: "Token is missing" });
   }
 
   jwt.verify(token, "your_secret_key", (err, decoded) => {
     if (err) {
-      return res.status(403).json({ message: "Token is not valid" });
+      return res
+        .status(403)
+        .json({ message: "Token is not valid. Please log in again." });
     }
     req.userId = decoded.userId;
     next();
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const axios = require("axios");
 const cors = require("cors");
 const validToken = require("./security/validToken");
 
@@ -170,6 +171,12 @@ app.get("/events/:year/:month", validToken, async (req, res) => {
       "date.month": month,
     });
 
+    const countryCode = "UA"; // Код країни України
+    const holidaysResponse = await axios.get(
+      `https://date.nager.at/api/v3/PublicHolidays/${year}/${countryCode}`
+    );
+    const holidays = holidaysResponse.data;
+    console.log(holidays);
     const daysInMonth = new Date(
       year,
       validMonthNames.indexOf(month) + 1,
@@ -191,25 +198,6 @@ app.get("/events/:year/:month", validToken, async (req, res) => {
       validMonthNames.indexOf(month),
       1
     ).getDay();
-    let startDayIndex = (firstDayIndex + 6) % 7;
-
-    const prevMonthLastDay = new Date(
-      year,
-      validMonthNames.indexOf(month),
-      0
-    ).getDate();
-    const daysFromPrevMonth = startDayIndex;
-
-    const prevMonthDays = Array.from({ length: daysFromPrevMonth }, (_, i) => ({
-      day: prevMonthLastDay - (daysFromPrevMonth - i - 1),
-      month:
-        month === "January"
-          ? validMonthNames[validMonthNames.length - 1]
-          : validMonthNames[(validMonthNames.indexOf(month) - 1 + 12) % 12],
-      year: month === "January" ? parseInt(year) - 1 : parseInt(year),
-      dayOfWeek: dayNames[(firstDayIndex - daysFromPrevMonth + i + 7) % 7],
-      event: null,
-    }));
 
     const currentMonthDays = Array.from({ length: daysInMonth }, (_, i) => {
       const day = i + 1;
@@ -220,6 +208,7 @@ app.get("/events/:year/:month", validToken, async (req, res) => {
         year: parseInt(year),
         dayOfWeek,
         event: null,
+        holiday: null,
       };
     });
 
@@ -241,11 +230,34 @@ app.get("/events/:year/:month", validToken, async (req, res) => {
         }
       });
     });
+    holidays.forEach((holiday) => {
+      const holidayDate = new Date(holiday.date);
+      currentMonthDays.forEach((day) => {
+        const dayDate = new Date(
+          day.year,
+          validMonthNames.indexOf(day.month),
+          day.day
+        );
+        if (holidayDate.toDateString() === dayDate.toDateString()) {
+          day.holiday = {
+            id: holiday.name,
+            title: holiday.localName,
+            description: holiday.name,
+            color: "#ffb3c1",
+            date: {
+              year: holidayDate.getFullYear(),
+              month: validMonthNames[holidayDate.getMonth()],
+              day: holidayDate.getDate(),
+            },
+          };
+        }
+      });
+    });
 
     const response = {
       year: parseInt(year),
       month,
-      days: [...prevMonthDays, ...currentMonthDays],
+      days: [...currentMonthDays],
     };
 
     res.json(response);
@@ -340,6 +352,169 @@ app.put("/events/:eventId", validToken, async (req, res) => {
     res.status(200).json(response);
   } catch (err) {
     console.error("Error updating event:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.get("/events/:title", validToken, async (req, res) => {
+  const { title } = req.params;
+  const userId = req.userId;
+  try {
+    const event = await Event.findOne({ title, userId });
+    if (!event) {
+      return res.status(404).send("Event not found");
+    }
+
+    // Отримати рік та місяць знайденої події
+    const { year, month } = event.date;
+
+    // Отримати дані за вказаний рік та місяць
+    const events = await Event.find({ "date.year": year, "date.month": month });
+
+    const validMonthNames = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+
+    const daysInMonth = new Date(
+      year,
+      validMonthNames.indexOf(month) + 1,
+      0
+    ).getDate();
+
+    const dayNames = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    const firstDayIndex = new Date(
+      year,
+      validMonthNames.indexOf(month),
+      1
+    ).getDay();
+
+    const currentMonthDays = Array.from({ length: daysInMonth }, (_, i) => {
+      const day = i + 1;
+      const dayOfWeek = dayNames[(firstDayIndex + i) % 7];
+      return {
+        day,
+        month,
+        year: parseInt(year),
+        dayOfWeek,
+        event: null,
+      };
+    });
+
+    events.forEach((event) => {
+      currentMonthDays.forEach((day) => {
+        if (
+          day.year === event.date.year &&
+          day.month === event.date.month &&
+          day.day === event.date.day &&
+          !day.event
+        ) {
+          day.event = {
+            id: event._id,
+            title: event.title,
+            description: event.description,
+            color: event.color,
+            date: event.date,
+          };
+        }
+      });
+    });
+
+    // Підготовка відповіді
+    const response = {
+      year: parseInt(year),
+      month,
+      days: [...currentMonthDays],
+    };
+
+    res.json(response);
+  } catch (err) {
+    console.error("Error fetching events by title:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.put("/events/update-date/:eventId", validToken, async (req, res) => {
+  const { eventId } = req.params;
+  const userId = req.userId;
+  const { newDate } = req.body;
+
+  try {
+    const existingEvent = await Event.findOne({
+      userId: userId,
+      "date.year": newDate.year,
+      "date.month": newDate.month,
+      "date.day": newDate.day,
+      _id: { $ne: eventId }, // Перевірка, щоб eventId не збігався з поточною подією
+    });
+
+    if (existingEvent) {
+      return res
+        .status(400)
+        .json({ message: "User already has an event for this date" });
+    }
+
+    const event = await Event.findOneAndUpdate(
+      {
+        _id: eventId,
+        userId: userId,
+      },
+      {
+        date: newDate,
+      },
+      { new: true }
+    );
+
+    if (!event) {
+      return res.status(404).json({ message: "Event not found" });
+    }
+
+    // Отримати назву дня тижня нової дати
+    const eventDate = new Date(
+      newDate.year,
+      dayNames.indexOf(newDate.month),
+      newDate.day
+    );
+    const dayOfWeek = dayNames[eventDate.getDay()];
+
+    const eventData = {
+      id: event._id,
+      title: event.title,
+      description: event.description,
+      date: event.date,
+      color: event.color,
+    };
+
+    const response = {
+      day: newDate.day,
+      month: newDate.month,
+      year: newDate.year,
+      dayOfWeek: dayOfWeek,
+      event: eventData,
+    };
+
+    res.status(200).json(response);
+  } catch (err) {
+    console.error("Error updating event date:", err);
     res.status(500).send("Internal Server Error");
   }
 });
